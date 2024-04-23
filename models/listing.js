d9d529bd3153dcf405b3be170c33ccd3bba3ba37
Arshlankhann <arshlank894@gmail.com>
2024-04-23 13:44:01 +0530

    first commit

---
+++ models/listing.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        require: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        set: (v) =>
+            v === ""
+                ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+                : v,
+        // image: {
+        //     filename: { type: String },
+        //     url: { type: String }
+        // },
+    },
+    price: Number,
+    location: String,
+    country: String,
+})
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
+
+
+
